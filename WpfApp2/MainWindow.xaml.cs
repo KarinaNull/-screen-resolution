ece3d5926c51b6c628fa1c8c68b91a9a26c21a10
Karina Nafanova <karinanull@gmail.com>
2020-12-17 01:41:22 +0300

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double width = SystemParameters.PrimaryScreenWidth / SystemParameters.CaretWidth;
+            double height = SystemParameters.PrimaryScreenHeight / SystemParameters.CaretWidth;
+            int WidthConvert = (int)width;
+            int HeightConvert = (int)height;
+            MessageBox.Show($"{WidthConvert}x{HeightConvert}");
+        }
+    }
+}
